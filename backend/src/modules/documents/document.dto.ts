ddebc4cc338eb8bc65a1d9609b6fa1be9d8ab7b9
ramSWS6 <ramsankar@softworthsolutions.in>
2026-06-12 11:07:49 +0530

    Initial backend implementation

---
+++ backend/src/modules/documents/document.dto.ts
@@ -0,0 +1,3 @@
+import type {components} from "../../generated/api-types.ts";
+export type UploadSignatureRequestDto = components["schemas"]["UploadSignatureRequest"];
+export type UploadSignatureResponseDto = components["schemas"]["CreateDocumentResponse"];
